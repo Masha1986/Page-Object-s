21c4811fd48c7429853a5336968eb213da26c344
Masha1986 <9312613800@mail.ru>
2022-03-16 11:23:00 +0300

    Add new files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Page-Object-s
-[![Java CI with Gradle](https://github.com/Masha1986/Page-Object-s/actions/workflows/gradle.yml/badge.svg)](https://github.com/Masha1986/Page-Object-s/actions/workflows/gradle.yml)
--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -1,48 +1,45 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import lombok.val;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
+    private SelenideElement heading = $("[data-test-id=dashboard]");
+    private SelenideElement buttonFirst = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] button");
+    private SelenideElement buttonSecond = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] button");
 
-  private SelenideElement heading = $("[data-test-id=dashboard]");
-  private SelenideElement actionCard1 = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] button");
-  private SelenideElement actionCard2 = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] button");
-  private SelenideElement notification = $("[data-test-id=error-notification]").$(withText("Ошибка"));
-  private final String balanceStart = "баланс: ";
-  private final String balanceFinish = " р.";
+    private ElementsCollection cards = $$(".list__item");
+    private final String balanceBefore = "баланс: ";
+    private final String balanceAfter = " р.";
 
-  public DashboardPage() {
-    heading.shouldBe(visible);
-  }
-
-  public Payments topUpCard(int card) {
-    if (card == 1) {
-      actionCard1.click();
+    public DashboardPage() {
+        heading.shouldBe(visible);
     }
-    else if (card == 2) {
-      actionCard2.click();
+
+    public int getCardBalance(int i) {
+        val text = cards.get(i).text();
+        return extractBalance(text);
     }
-    return new Payments();
-  }
 
-  public int getCardBalance(String id) {
-    val text = $("[data-test-id='" + id + "']").text();
-    return extractBalance(text);
-  }
+    private int extractBalance(String text) {
+        val start = text.indexOf(balanceBefore);
+        val finish = text.indexOf(balanceAfter);
+        val value = text.substring(start + balanceBefore.length(), finish);
+        return Integer.parseInt(value);
+    }
 
-  private int extractBalance(String text) {
-    val start = text.indexOf(balanceStart);
-    val finish = text.indexOf(balanceFinish);
-    val value = text.substring(start + balanceStart.length(), finish);
-    return Integer.parseInt(value);
-  }
+    public CardPage personFirstCard() {
+        buttonFirst.click();
+        return new CardPage();
+    }
 
-  public SelenideElement getNotificationVisible() {
-    return notification.shouldBe(visible);
-  }
+    public CardPage personSecondCard() {
+        buttonSecond.click();
+        return new CardPage();
+    }
 }
